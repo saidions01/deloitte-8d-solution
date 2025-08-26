097f7d925081484bd039a4cf52262b719248d3a3
saidions01 <101123304+saidions01@users.noreply.github.com>
2025-08-26 21:37:50 +0100

    allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts : ["deloitte-8d-solution.onrender.com"],
   },
   plugins: [
     react(),
